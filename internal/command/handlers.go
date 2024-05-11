cfff6b12e2cefd5e12c65cfcdccc5a8aa8af517f
Eddort <webisforce@gmail.com>
2024-05-11 14:23:46 +0200

    fix: foundry

--- internal/command/handlers.go
+++ internal/command/handlers.go
@@ -1,12 +1,16 @@
 package command
 
+import "strings"
+
 func nodeCommandHandler(tag string, commandName string, additionalArgs []string) (string, []string) {
 	return "node:" + tag, append([]string{commandName}, additionalArgs...)
 }
 
 func foundryCommandHandler(tag string, commandName string, additionalArgs []string) (string, []string) {
-	// args := escapeArgs(additionalArgs)
-	return "ghcr.io/foundry-rs/foundry:" + tag, append([]string{commandName}, additionalArgs...)
+	args := escapeArgs(additionalArgs)
+	fullCommand := append([]string{commandName}, args...)
+	return "ghcr.io/foundry-rs/foundry:" + tag, []string{strings.Join(fullCommand, " ")}
+	// return "ghcr.io/foundry-rs/foundry:" + tag, append([]string{commandName}, additionalArgs...)
 }
 
 func pythonCommandHandler(tag string, commandName string, additionalArgs []string) (string, []string) {
