3922314b7c4fc9dad5366115521e0060b43b22d6
Eddort <webisforce@gmail.com>
2024-05-11 14:07:43 +0200

    fix: aliases

--- internal/command/handlers.go
+++ internal/command/handlers.go
@@ -1,20 +1,18 @@
 package command
 
-import "strings"
-
-func nodeCommandHandler(tag string, additionalArgs []string) (string, []string) {
-	return "node:" + tag, append([]string{"node"}, additionalArgs...)
+func nodeCommandHandler(tag string, commandName string, additionalArgs []string) (string, []string) {
+	return "node:" + tag, append([]string{commandName}, additionalArgs...)
 }
 
-func foundryCommandHandler(tag string, additionalArgs []string) (string, []string) {
-	args := escapeArgs(additionalArgs)
-	return "ghcr.io/foundry-rs/foundry:" + tag, []string{strings.Join(args, " ")}
+func foundryCommandHandler(tag string, commandName string, additionalArgs []string) (string, []string) {
+	// args := escapeArgs(additionalArgs)
+	return "ghcr.io/foundry-rs/foundry:" + tag, append([]string{commandName}, additionalArgs...)
 }
 
-func pythonCommandHandler(tag string, additionalArgs []string) (string, []string) {
-	return "python:" + tag, append([]string{"python"}, additionalArgs...)
+func pythonCommandHandler(tag string, commandName string, additionalArgs []string) (string, []string) {
+	return "python:" + tag, append([]string{commandName}, additionalArgs...)
 }
 
-func rubyCommandHandler(tag string, additionalArgs []string) (string, []string) {
-	return "ruby:" + tag, append([]string{"ruby"}, additionalArgs...)
+func rubyCommandHandler(tag string, commandName string, additionalArgs []string) (string, []string) {
+	return "ruby:" + tag, append([]string{commandName}, additionalArgs...)
 }
--- internal/command/helper.go
+++ internal/command/helper.go
@@ -1,29 +1,28 @@
 package command
 
 import (
-	"strconv"
 	"strings"
 )
 
-func escapeArgs(args []string) []string {
-	var processedArgs []string
+// func escapeArgs(args []string) []string {
+// 	var processedArgs []string
 
-	for _, arg := range args {
-		_, errInt := strconv.ParseInt(arg, 10, 64)
-		_, errFloat := strconv.ParseFloat(arg, 64)
-		_, errBool := strconv.ParseBool(arg)
+// 	for _, arg := range args {
+// 		_, errInt := strconv.ParseInt(arg, 10, 64)
+// 		_, errFloat := strconv.ParseFloat(arg, 64)
+// 		_, errBool := strconv.ParseBool(arg)
 
-		if errInt == nil || errFloat == nil || errBool == nil {
+// 		if errInt == nil || errFloat == nil || errBool == nil {
 
-			processedArgs = append(processedArgs, arg)
-		} else {
+// 			processedArgs = append(processedArgs, arg)
+// 		} else {
 
-			processedArgs = append(processedArgs, "\""+arg+"\"")
-		}
-	}
+// 			processedArgs = append(processedArgs, "\""+arg+"\"")
+// 		}
+// 	}
 
-	return processedArgs
-}
+// 	return processedArgs
+// }
 
 func parseBaseCommand(baseCommand string) (string, string) {
 	x := strings.Split(baseCommand, ":")
