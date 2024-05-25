79709c01dad575538bd9ad6e04a35455c5ec67e4
Eddort <webisforce@gmail.com>
2024-05-25 17:23:52 +0200

    feat: programs from config

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -7,12 +7,10 @@ import (
 	"os"
 )
 
-func myUsage() {
-	fmt.Println(getHelpMessage())
-}
-
-func Parse() ([]string, config.CLI) {
-	flag.Usage = myUsage
+func Parse(configuration config.ProgramConfig) ([]string, config.CLI) {
+	flag.Usage = func() {
+		fmt.Println(getHelpMessage(configuration))
+	}
 
 	IsSelectMode := flag.Bool("select", false, "Interactive selection of the required application version")
 
@@ -22,7 +20,7 @@ func Parse() ([]string, config.CLI) {
 	commandArgs := flag.Args()
 
 	if len(commandArgs) < 1 {
-		fmt.Println(getHelpMessage())
+		fmt.Println(getHelpMessage(configuration))
 		os.Exit(1)
 	}
 
