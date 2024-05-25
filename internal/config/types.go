a429c01df476994c1fd57370ab0f9263054fbf28
Eddort <webisforce@gmail.com>
2024-05-25 20:43:44 +0200

    feat: update config type

--- internal/config/types.go
+++ internal/config/types.go
@@ -5,18 +5,28 @@ type CLI struct {
 	FileIgnores  []string
 }
 
+type Hook struct {
+	Command  string   `mapstructure:"command" validate:"required"`
+	Settings Settings `mapstructure:"settings"`
+}
+
 type Program struct {
-	Name string `mapstructure:"name" validate:"required"`
-	// Aliases     []string `mapstructure:"aliases"`
-	Image       string `mapstructure:"image" validate:"required"`
-	Command     string `mapstructure:"command"`
-	Serializer  string `yaml:"serializer" default:"default" validate:"serializer"`
-	Description string `mapstructure:"description"`
-	Category    string `mapstructure:"category"`
+	Name        string   `mapstructure:"name" validate:"required"`
+	Image       string   `mapstructure:"image" validate:"required"`
+	Command     string   `mapstructure:"command"`
+	Serializer  string   `mapstructure:"serializer" validate:"oneof='' default string testhandler"`
+	Description string   `mapstructure:"description"`
+	Category    string   `mapstructure:"category"`
+	Hooks       []Hook   `mapstructure:"hooks" validate:"dive"`
+	Settings    Settings `mapstructure:"settings"`
+}
+
+type Settings struct {
+	Net         string   `mapstructure:"net" validate:"oneof='' none host bridge"`
+	IgnorePaths []string `mapstructure:"ignore_paths"`
 }
 
 type ProgramConfig struct {
 	Programs []Program `mapstructure:"programs" validate:"required,dive"`
+	Settings Settings  `mapstructure:"settings"`
 }
-
-var validSerializers = []string{"default", "string", "testhandler"}
